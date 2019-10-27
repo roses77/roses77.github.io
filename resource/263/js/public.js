b19c0f9e046c2b1033d6a4f9d4de3ebb06759f84
roses77 <504115485@qq.com>
2019-10-28 04:19:24 +0800

    Add files via upload

---
+++ resource/263/js/public.js
@@ -0,0 +1,13 @@
+function getQueryString(name) { 
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i"); 
+	var r = window.location.search.substr(1).match(reg); 
+	if (r != null) {return unescape(r[2]);}else{ return 0}; 
+}
+function isWeiXin(){
+    var ua = window.navigator.userAgent.toLowerCase();
+    if(ua.match(/MicroMessenger/i) == 'micromessenger'){
+        return true;
+    }else{
+        return false;
+    }
+}
